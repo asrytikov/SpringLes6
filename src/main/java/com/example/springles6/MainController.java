711c254c4e0b5dab22df122fd6fb380f26a409ed
asrytikov <asrytikov@gmail.com>
2023-04-10 21:22:06 +0400

    Initial commit

---
+++ src/main/java/com/example/springles6/MainController.java
@@ -0,0 +1,2 @@
+package com.example.springles6;public class MainController {
+}
---
+++ src/main/java/com/example/springles6/SpringLes6Application.java
@@ -0,0 +1,13 @@
+package com.example.springles6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLes6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLes6Application.class, args);
+    }
+
+}
